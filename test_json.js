e7dfcd9d96949adf7e3a8af8a94f1b13fa16d842
ytlee64 <ytlee64@naver.com>
2019-01-22 11:06:14 +0900

    test sample

--- test_json.js
+++ test_json.js
@@ -8,7 +8,7 @@ var libjson = require('./lib/libjson.js');
 var json_string = fs.readFileSync('test.json', 'utf8');
 var json_object=JSON.parse(json_string);
 
-//libjson.print(json_object);
+libjson.print(json_object);
 
 
 console.log(libjson.getvalue(json_object,["repository","type"]));
@@ -16,13 +16,13 @@ console.log(libjson.getvalue(json_object,["repository","type"]));
 // https://eos.greymass.com/v1/chain/get_info
  
 
-var post_req = https.request('https://eos.greymass.com/v1/chain/get_info', function(res) {
-    res.setEncoding('utf8');
-    res.on('data', function (result) {
-        var json_object=JSON.parse(result);
-        libjson.print(json_object);
-    });
-});
+// var post_req = https.request('https://eos.greymass.com/v1/chain/get_info', function(res) {
+//     res.setEncoding('utf8');
+//     res.on('data', function (result) {
+//         var json_object=JSON.parse(result);
+//         libjson.print(json_object);
+//     });
+// });
 
-post_req.end();
+// post_req.end();
 
